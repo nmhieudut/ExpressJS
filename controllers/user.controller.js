9c46cc95c87b89f61f8c42b6bfd10e410eb97d27
tuanconbu <hieutk5@gmail.com>
2020-08-14 16:39:18 +0700

    first commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,35 @@
+var db = require('../db')
+var shortid = require('shortid');
+
+module.exports.index = function (req, res) {
+    res.render("users/index", {
+        users: db.get("users").value()
+    })
+}
+module.exports.search = function (req, res) {
+    var q = req.query.q;
+    var users = db.get("users").value();
+    var matchedUser = users.filter(x => x.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+    res.render('users/index', {
+        users: matchedUser,
+        value: q
+    })
+}
+module.exports.create = function (req, res) {
+    res.render('users/create')
+}
+module.exports.get = function (req, res) {
+    var id = req.params.id;
+    var user = db.get("users").find({ id: id }).value();
+    console.log(user);
+    res.render("users/view", {
+        user: user
+    })
+}
+module.exports.postCreate = function (req, res) {
+    req.body.id = shortid.generate();
+    db.get('users')
+        .push(req.body)
+        .write()
+    res.redirect('/users')
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var port = 3000;
+var bodyParser = require('body-parser')
+var shortid = require('shortid');
+var userRoute = require('./routes/user.route');
+var db = require('./db')
+
+var app = express();
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.get('/', (req, res) =>
+    res.render("index")
+);
+app.use('/users',userRoute);
+
+
+app.listen(port, () => { console.log("listening on port " + port) });
